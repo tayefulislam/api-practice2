69cede1f5f09a770daa4f283df2aa158a93e11d3
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-24 17:31:07 +0600

    meal db add

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,14 +1,22 @@
 const searchFood = () => {
-
     const inputField = document.getElementById('input-data');
     const inputData = inputField.value;
-
-
     inputField.value = '';
     console.log(inputData);
 
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputData}`)
+        .then(response => response.json())
+        .then(data => showData(data.meals))
+
+}
+
+const showData = (meals) => {
+
+    meals.forEach(meal => {
+
+        console.log(meal.strMeal)
 
 
 
-
+    });
 }
