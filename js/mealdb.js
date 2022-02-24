33a35d4bc47feacc25c591d5719dbe45e8f4b514
tayeful912 <kazitayefulislam12@gmail.com>
2022-02-24 16:03:38 +0600

    mealdb - search option add

---
+++ js/mealdb.js
@@ -0,0 +1,14 @@
+const searchFood = () => {
+
+    const inputField = document.getElementById('input-data');
+    const inputData = inputField.value;
+
+
+    inputField.value = '';
+    console.log(inputData);
+
+
+
+
+
+}
